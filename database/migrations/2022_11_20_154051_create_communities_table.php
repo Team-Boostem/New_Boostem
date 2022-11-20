f751e4007003255f8e62705f8ba381d0090bbdff
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-11-20 21:35:12 +0530

    community migration and model created

---
+++ database/migrations/2022_11_20_154051_create_communities_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('communities', function (Blueprint $table) {
+            $table->string('id');
+            $table->primary('id');
+            $table->string('name');
+            $table->string('community_name')->unique();
+            $table->string('tagline');
+            $table->string('email')->unique();
+            $table->string('contact')->nullable();
+            $table->string('website')->nullable();
+            $table->string('college')->nullable();
+            $table->string('country')->nullable();
+            $table->string('state')->nullable();
+            $table->string('city')->nullable();
+            $table->longText('description')->nullable();
+            $table->longText('about')->nullable();
+            $table->date('founded_at')->nullable();
+            $table->json('domains')->nullable();
+            $table->json('tags')->nullable();
+            $table->json('field')->nullable();
+            $table->json('socials')->nullable();
+            $table->string('logo_photo_path', 2048)->nullable();
+            $table->string('banner_photo_path', 2048)->nullable();
+            $table->string('creator')->nullable();
+            $table->json('admin')->nullable();
+            $table->boolean('active')->default(true);
+            $table->boolean('suspended')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('communities');
+    }
+};
