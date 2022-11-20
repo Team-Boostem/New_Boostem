940753b3a32c2492637e337fe1b611b39e878200
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-11-20 12:34:16 +0530

    get method to logout added

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('logout', function (){
+    auth()->logout();
+    Session()->flush(); return Redirect::to('/');
+})->name('logout');
 
 Route::middleware([
     'auth:sanctum',
