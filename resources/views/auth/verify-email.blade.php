e2960f2a86768968c185545a6cfed2d1175a489f
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-11-22 16:56:20 +0530

    email vatrification page done

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,4 +1,112 @@
-<x-guest-layout>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <title>Forgot Password | Boostem</title>
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link rel="stylesheet" href="{{ asset('public/css/style.css') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('public/icons/logo.png') }}">
+    <link href="{{ asset('public/cork/html/layouts/collapsible-menu/css/light/loader.css') }}" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ asset('public/cork/html/layouts/collapsible-menu/css/dark/loader.css') }}" rel="stylesheet"
+        type="text/css" />
+    <script src="{{ asset('public/cork/html/layouts/collapsible-menu/loader.js') }}"></script>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <link href="{{ asset('public/cork/html/src/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('public/cork/html/layouts/collapsible-menu/css/light/plugins.css') }}" rel="stylesheet"
+        type="text/css" />
+    <link href="{{ asset('public/cork/html/layouts/collapsible-menu/css/dark/plugins.css') }}" rel="stylesheet"
+        type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <link href="{{ asset('public/cork/html/src/assets/css/light/authentication/auth-boxed.css') }}" rel="stylesheet"
+        type="text/css" />
+
+</head>
+
+<body class="form">
+
+
+    <!-- BEGIN LOADER -->
+    <div id="load_screen">
+        <div class="loader">
+            <div class="loader-content">
+                <div class="spinner-grow align-self-center"></div>
+            </div>
+        </div>
+    </div>
+    <!--  END LOADER -->
+
+    <div class="auth-container d-flex h-100">
+
+        <div class="container mx-auto align-self-center">
+
+            <div class="row">
+
+                <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-8 col-12 d-flex flex-column align-self-center mx-auto">
+                    <div class="card mt-3 mb-3">
+                        <div class="card-body">
+
+                            <div class="row">
+                                <div class="col-md-12 mb-3">
+
+                                    <h2>Email Varification</h2>
+                                    <p>Before continuing, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.</p>
+                                    @if (session('status') == 'verification-link-sent')
+                                    <div class="alert alert-success" role="alert">
+                                        A new verification link has been sent to the email address you provided in your profile settings.
+                                    </div>
+                                    @endif
+                                    @if ($errors->any())
+                                        <div class="alert alert-danger" role="alert">
+                                            <x-jet-validation-errors class="" />
+                                        </div>
+                                    @endif
+
+                                </div>
+                                <form method="POST" action="{{ route('verification.send') }}">
+                                    @csrf
+                                    
+                                    <div class="col-12">
+                                        <div class="mb-4">
+                                            <button class="btn btn-secondary w-100">Resend Verification Email</button>
+                                        </div>
+                                    </div>
+                                    
+                                    
+                                </form>
+                                <div class="col-12">
+                                    <div class="mb-4">
+                                        <button class="btn btn-light w-100">logout</button>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+    <script src="{{ asset('public/cork/html/src/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('public/js/script.js') }}"></script>
+    <!-- END GLOBAL MANDATORY SCRIPTS -->
+    <script></script>
+
+</body>
+
+</html>
+
+
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -42,4 +150,4 @@
             </div>
         </div>
     </x-jet-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
