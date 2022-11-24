10452e3cbfb3373e4ee6c1d5bad828ceef6161df
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-11-25 01:51:21 +0530

    healper added

---
+++ app/helpers.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Models\Community;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+// use Illuminate\Support\Facades\DB;
+
+//get all communities where creator is equal to auth()->user()->user_id
+// $mycommunities = Community::where( 'creator', Auth::user()->user_id )->get();
+// $mycommunities->setAsGlobal();
+
+
+?>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -71,12 +71,14 @@
                             <h6 class="d-flex justify-content-between"><span class="align-self-center">Messages</span> <span class="badge badge-primary">9 Unread</span></h6>
                         </div>
                         <div class="notification-scroll" style="height: auto">
+
+                            {{-- @foreach ($mycommunities as $mycommunity)
                             <div class="dropdown-item">
                                 <div class="media server-log">
                                     <img src="../src/assets/img/profile-16.jpeg" class="img-fluid me-2" alt="avatar">
                                     <div class="media-body">
                                         <div class="data-info">
-                                            <h6 class="">Kara Young</h6>
+                                            <h6 class="">{{ $mycommunity->name }}</h6>
                                             <p class="">1 hr ago</p>
                                         </div>
                                         
@@ -86,39 +88,8 @@
                                     </div>
                                 </div>
                             </div>
-                            
-                            <div class="dropdown-item">
-                                <div class="media ">
-                                    <img src="../src/assets/img/profile-15.jpeg" class="img-fluid me-2" alt="avatar">
-                                    <div class="media-body">
-                                        <div class="data-info">
-                                            <h6 class="">Daisy Anderson</h6>
-                                            <p class="">8 hrs ago</p>
-                                        </div>
+                            @endforeach --}}
 
-                                        <div class="icon-status">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="dropdown-item">
-                                <div class="media file-upload">
-                                    <img src="../src/assets/img/profile-21.jpeg" class="img-fluid me-2" alt="avatar">
-                                    <div class="media-body">
-                                        <div class="data-info">
-                                            <h6 class="">Oscar Garner</h6>
-                                            <p class="">14 hrs ago</p>
-                                        </div>
-
-                                        <div class="icon-status">
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            
                             <div class="drodpown-title notification mt-2">
                                 <a href="{{ route('create.community') }}">
                                 <h6 class="d-flex justify-content-between"><span class="align-self-center">Create Community</span> <span class="badge badge-secondary">+ New</span></h6>
