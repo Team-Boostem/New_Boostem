5a722fe6892fe5b4b87d541e19511b6b8934a810
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-11-28 15:40:56 +0530

    model of page view updated

--- app/Models/PageView.php
+++ app/Models/PageView.php
@@ -4,16 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Community;
+// use App\Models\Startup;
+
 
 class PageView extends Model
 {
     use HasFactory;
-    
-    public static function boot()
+   
+    protected $table = 'page_views';
+
+    protected $appends = [
+        // 'profile_photo_url',
+        'user',
+        'profile',
+    ];
+
+    public function getUserAttribute()
     {
-        parent::boot();
-        static::creating(function ($user) {
-            $user->id = (string) Str::uuid();
-        });
+        return User::where('user_id', $this->user_id)->select('user_id', 'name', 'username', 'profile_photo_path')->first();
+    }
+
+    public function getProfileAttribute()
+    {
+        if ($this->page == 'user/{username}') {
+            return User::where('user_id', $this->profile_id)->select('user_id', 'name', 'username', 'profile_photo_path')->first();
+        } else if ($this->page == 'community/{username}') {
+            return Community::where('id', $this->profile_id)->select('id', 'name', 'username', 'logo_photo_path')->first();
+        // } else if ($this->page == 'startup/{username}') {
+        //     return Startup::where('id', $this->profile_id)->select('id', 'name', 'username', 'logo_photo_path')->first();
+        }
+        return null;
     }
 }
