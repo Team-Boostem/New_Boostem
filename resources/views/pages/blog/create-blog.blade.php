3935a2dfd1c399b1eac9694343faeacad37e9715
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-12-17 11:59:00 +0530

    BLOG VIEW PAGES INISIATED

---
+++ resources/views/pages/blog/create-blog.blade.php
@@ -0,0 +1,23 @@
+{{-- push title --}}
+@push('title')
+    <title>Create Blog | Boostem</title>
+@endpush
+
+{{-- push styles --}}
+@push('styles')
+<style>
+    
+</style>
+@endpush
+
+{{-- push scripts --}}
+@push('scripts')
+@endpush
+
+{{-- extend and yield content --}}
+@extends('layouts/community-dashboard')
+@section('content')
+   <div class="main-box">
+    test page
+   </div>
+@endsection
