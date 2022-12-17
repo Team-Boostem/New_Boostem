457537026949d9870efe6e7dd20fcab30d358f09
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-12-17 20:04:55 +0530

    view page build

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -7,39 +7,59 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Auth;
 
-class BlogController extends Controller
-{
+class BlogController extends Controller {
     public function blogList() {
         //GET ALL BLOGs
-        // $blogs = Blog::all();
-        // return view('pages.blog.list-blog', compact('blogs'));
-        $blogs = DB::table('blogs')
-            ->join('users', 'blogs.creator', '=', 'users.user_id')
-            ->select('blogs.*','users.user_id', 'users.name', 'users.profile_photo_path')
-            ->get();
-        return view('pages.blog.list-blog', compact('blogs'));
+        $blogs = DB::table( 'blogs' )
+        ->join( 'users', 'blogs.creator', '=', 'users.user_id' )
+        ->select( 'blogs.*', 'users.user_id', 'users.name', 'users.profile_photo_path' )
+        ->get();
+        return view( 'pages.blog.list-blog', compact( 'blogs' ) );
     }
 
-    public function blogView($slug) {
-        return view('pages.blog.view-blog');
+    public function blogView( $blog_slug ) {
+        //GET BLOG BY SLUG
+        $blog = DB::table( 'blogs' )
+        ->join( 'users', 'blogs.creator', '=', 'users.user_id' )
+        ->select( 'blogs.*', 'users.user_id', 'users.name', 'users.username', 'users.profile_photo_path' )
+        ->where( 'slug', $blog_slug )
+        ->first();
+        //convert category into array
+        $category = $blog->category;
+        $cat_data = explode( ',', $category );
+        $cat_array_data = array();
+        foreach ( $cat_data as $cat ) {
+            array_push($cat_array_data,$cat);
+        }
+        //convert tags into array
+        $tags = $blog->tags;
+        $tags_data = explode( ',', $tags );
+        $tags_array_data = array();
+        foreach ( $tags_data as $tag ) {
+            array_push($tags_array_data,$tag);
+        }
+        // dd( $cat_array_data );
+        return view( 'pages.blog.view-blog', compact( 'blog','tags_array_data','cat_array_data' ) );
     }
+
     public function blogCreate() {
-        return view('pages.blog.create-blog');
+        return view( 'pages.blog.create-blog' );
     }
-    public function blogCreatePost( Request $request) {
-        $request->validate([
+
+    public function blogCreatePost( Request $request ) {
+        $request->validate( [
             'title' => 'required',
             'description' => 'required',
             'slug' => 'required',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg',
-        ]);
+        ] );
         //get image save it in storage and get the path
-        if ($request->hasFile('image')) {
-            $image = $request->file('image');
+        if ( $request->hasFile( 'image' ) ) {
+            $image = $request->file( 'image' );
             $name = time().'.'.$image->getClientOriginalExtension();
             //store in storage blog folder
             $path = 'storage/covers/blogs/';
-            $image->move($path, $name);
+            $image->move( $path, $name );
             // $this->save();
         }
         //convert category into csv
@@ -67,11 +87,13 @@ class BlogController extends Controller
         $blog->creator_type = 'c';
         $blog->type = $yourval;
         $blog->save();
-        return redirect()->route('blog');
+        return redirect()->route( 'blog' );
 
     }
+
     public function blogEdit() {
     }
+
     public function blogEditPost() {
     }
 }
--- resources/views/pages/blog/view-blog.blade.php
+++ resources/views/pages/blog/view-blog.blade.php
@@ -1,13 +1,42 @@
 {{-- push title --}}
 @push('title')
     <title>Blog | Boostem</title>
+    <!--  BEGIN CUSTOM STYLE FILE  -->
+    <link href="{{ asset('public/cork/html/src/assets/css/light/elements/custom-pagination.css') }}" rel="stylesheet"
+        type="text/css" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('public/cork/html/src/assets/css/light/apps/blog-post.css') }}">
+
+    <!--  END CUSTOM STYLE FILE  -->
 @endpush
 
 {{-- push styles --}}
 @push('styles')
-<style>
-    
-</style>
+    <style>
+        .header-section{
+            margin: 1rem 4.5rem;
+        }
+        .image-container img{
+            width: 100%;
+            max-height: 600px;
+            object-fit: cover;
+            border-radius: 20px;
+            overflow: hidden;
+            border-bottom-left-radius: 0;
+            border-bottom-right-radius: 0;
+        }
+        .creater-img img{
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            object-fit: cover;
+        }
+        .creater-profail{
+            margin: 1rem 0;
+            display: flex;
+            flex-direction: row;
+        }
+        
+    </style>
 @endpush
 
 {{-- push scripts --}}
@@ -17,7 +46,59 @@
 {{-- extend and yield content --}}
 @extends('layouts/community-dashboard')
 @section('content')
-   <div class="main-box">
-    test page
-   </div>
+    <div class="main-box">
+        <div class="middle-content container-xxl p-0">
+
+            <div class="row layout-top-spacing">
+
+                <div class="col-xxl-12 col-xl-12 col-lg-12 col-md-12 col-sm-12 mb-4">
+
+                    <div class="single-post-content">
+
+                        <div class="header-section">
+                            <div class="image-container">
+                                <img src="{{ $blog->image }}" alt="">
+                            </div>
+                            <div class="creater-profail flex">
+                                <div class="creater-img">
+                                    <img src="/{{ $blog->profile_photo_path }}" alt="">
+                                </div>
+                                <div class="creater-info">
+                                    {{-- <a href="{{  }}"> --}}
+                                        <h5 class="creater-name">{{ $blog->name }}</h5>
+                                    <p class="creater-name">{{ $blog->username }}</>
+                                    {{-- </a> --}}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="post-content">
+                            {!! $blog->description !!}
+                        </div>
+
+                        <div class="post-info">
+
+                            <hr>
+
+                            <div class="post-tags mt-5">
+                                @foreach ($cat_array_data as $cat)
+                                <span class="badge badge-primary mb-2">{{ $cat }}</span>
+                                @endforeach
+                            </div>
+                            <div class="post-tags mt-5">
+                                @foreach ($tags_array_data as $tag)
+                                <span class="badge badge-primary mb-2">{{ $tag }}</span>
+                                @endforeach
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+    </div>
 @endsection
