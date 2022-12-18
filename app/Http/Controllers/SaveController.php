af70017354a196737f348428ca323a0e03e7c4ef
sumitchoudharySEM <sumitsem2004@gmail.com>
2022-12-18 22:43:20 +0530

    saves table and model created

---
+++ app/Http/Controllers/SaveController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Save;
+use Illuminate\Http\Request;
+
+class SaveController extends Controller
+{
+    
+}
---
+++ database/migrations/2022_12_18_164951_create_saves_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('saves', function (Blueprint $table) {
+            $table->string('id');
+            $table->primary('id');
+            $table->string('user_id');
+            $table->string('page_type'); //blog,event,post type
+            $table->string('page_id'); //blog_slug,event_id,post_id type
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('saves');
+    }
+};
