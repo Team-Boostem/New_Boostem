498d2152cd6fbadd559cbf124f1a73fdd8f8f23b
sumitchoudharySEM <sumitsem2004@gmail.com>
2023-02-03 15:39:57 +0530

    neawsletter done

--- resources/views/pages/leanding.blade.php
+++ resources/views/pages/leanding.blade.php
@@ -10,37 +10,142 @@
 {{-- push styles --}}
 @push('styles')
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+        .subscribe-container {
+            display: flex;
+            background-color: rgb(0, 132, 214);
+            padding: 3.5rem 2rem;
+            border-radius: 0.5rem;
+            justify-content: space-around;
+            box-shadow: rgba(129, 129, 129, 0.35) 0px 5px 15px;
+        }
+
+        .subscribe-container h1 {
+            font-size: 1.75rem;
+            margin: 0;
+            padding: 0;
+            margin: 0.5rem 0;
+            margin-top: 1rem;
+            font-family: 'Montserrat', sans-serif;
+            font-weight: 700;
+            color: rgb(255, 255, 255);
+        }
+
+        .join {}
+
+        .join p {
+            color: rgb(239, 239, 239);
+            margin: 0.5rem 0;
+            font-size: 1.1rem;
+            font-family: 'Montserrat', sans-serif;
+        }
+
+        .email-btn {
+            margin-bottom: 1rem;
+        }
+        .email{
+            width: 50%
+        }
+
+        .email-btn{
+            display: flex;
+            width: 90%;
+            justify-content: start;
+        }
+
+        .email .email-btn input {
+            width: 80%;
+            padding: 1rem 1rem;
+            border-radius: 0.25rem;
+            border: none;
+            box-shadow: rgba(180, 180, 180, 0.35) 0px 5px 15px;
+            font-size: 1rem;
+            text-align: start;
+        }
+
+        .btn {
+            border-radius: 0.25rem;
+            padding: 1.1rem 2rem;
+            border: none;
+            box-shadow: rgba(1, 29, 186, 0.548) 0px 5px 15px;
+            background-color: rgb(0, 89, 255);
+            color: white;
+        }
+
+        /* #underline {
+            border-bottom: 1px solid black;
+        } */
+
+        .privacy p {
+            color: rgb(212, 212, 212);
+            font-size: 0.85rem;
+        }
+
+        @media (max-width: 768px) {
+            .subscribe-container {
+                display: flex;
+                flex-direction: column;
+                padding: 1rem;
+            }
+            .subscribe-container h1 {
+               font-size: 1.2rem;
+            }
+            .subscribe-container p {
+               font-size: 0.8rem;
+            }
+            .join {
+                width: 100%;
+            }
+
+            .email {
+                width: 100%;
+            }
+
+            .email .email-btn {
+                width: 100%;
+            }
+            .email .email-btn input {
+                width: 100%;
+                padding: 0.4rem 0.9rem;
+                height: 3rem;
+            }
+
+            .btn {
+                width: 35%;
+                height: 3rem;
+                padding: 0.4rem 0.9rem;
+            }
+        }
     </style>
 @endpush
 
 {{-- push scripts --}}
 @push('scripts')
     <script type="text/javascript">
-        $("#newsletter_btn").click(function(){
-                console.log('hii')
-                //var email_id as the value of input with name newsletter 
-                var email_id = $("#newsletter_inp").val();
-                // send ajax request
-                $.ajax({
-                    url: "{{ route('newsletter.save') }}",
-                    type: "POST",
-                    data: {
-                        "_token": "{{ csrf_token() }}",
-                        "email_id": email_id,
-                    },
-                    success: function(response) {
-                        console.log(response);
-                        if (response.status == 200) {
-                            $("#newsletter-status").html(response);
+        $("#newsletter_btn").click(function() {
+            //var email_id as the value of input with name newsletter 
+            var email_id = $("#newsletter_inp").val();
+            // send ajax request
+            $.ajax({
+                url: "{{ route('newsletter.save') }}",
+                type: "POST",
+                data: {
+                    "_token": "{{ csrf_token() }}",
+                    "email_id": email_id,
+                },
+                success: function(response) {
+                    console.log(response);
+                    if (response.status == 200) {
+                        $("#newsletter-status").html(response);
 
-                        } else {
-                            $("#newsletter-status").html(response);
-                            $("#newsletter_row").hide();
-                        }
+                    } else {
+                        $("#newsletter-status").html(response);
+                        $(".subscribe-container").hide();
                     }
-                });
-            }
-        );
+                }
+            });
+        });
     </script>
 @endpush
 
@@ -49,37 +154,29 @@
 @section('content')
     <div class="main-box">
         <div id="search-status"></div>
-        {{-- <div>
-            <div class="fq-header-wrapper">
-                <div class="container">
-                    @if (subscribeNewsletterStatus())
-                    @else
-                        <div class="alert alert-success" id="newsletter-status"></div>
-                        <div class="row" id="newsletter_row">
-                            <div class="col-md-12 align-self-center order-md-0 order-1">
-                                <div class="faq-header-content">
-                                    <h1 class="mb-4">Subscribe to newsletter</h1>
-                                    <div class="row">
-                                        <div class="col-lg-5 mx-auto">
-                                            <div class="autocomplete-btn">
-                                                <input id="newsletter_inp" type="email" class="form-control"
-                                                    @auth
-                                                        value="{{ Auth::user()->email }}" 
-                                                    @endauth>
-                                                <button type="button" id="newsletter_btn"
-                                                    class="btn btn-primary">Subscribe</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <p class="mt-4 mb-0">Search instant answers & questions asked by popular users</p>
-                                </div>
-                            </div>
+        <div id="newsletter-status"></div>
+        <div>
+            @if (subscribeNewsletterStatus())
+            @else
+                <div class="subscribe-container">
+                    <div class="join">
+                        <h1>Join 200+ Subscriber</h1>
+                        <p>Stay in the loop with everything you need to know.</p>
+                    </div>
+                    <div class="email">
+                        <div class="email-btn">
+                            <input type="email"  id="newsletter_inp"
+                                @auth value="{{ Auth::user()->email }}" @endauth placeholder="Enter your email">
+                            <button class="btn" id="newsletter_btn">Subscribe</button>
                         </div>
-                    @endif
-
+                        <div class="privacy">
+                            <p>We care about your data in our <span id="underline">privacy policy.</span> </p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div> --}}
-        
+            @endif
+        </div>
+
+
     </div>
 @endsection
