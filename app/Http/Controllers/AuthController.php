6e4b1135c0ac95f39cf6d97ac76841335b6bd2e8
sumitchoudharySEM <sumitsem2004@gmail.com>
2023-02-04 11:47:57 +0530

    google auth fixed

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,8 +14,8 @@ class AuthController extends Controller {
 
     public function handdleGoogleCallBack() {
         $google_user = Socialite::driver( 'google' )->user();
-        $user = User::where( 'google_id', $google_user->getId() )->first();
 
+        $user = User::where( 'email', $google_user->email )->first();
         if ( !$user) {
             $user = User::create( [
                 'name' => $google_user->name,
@@ -25,8 +25,16 @@ class AuthController extends Controller {
             Auth::login( $user,true );
             return redirect()->route( 'dashboard' );
         } else {
-            Auth::login( $user,true );
-            return redirect()->route( 'dashboard' );
-        }
+            if ( $user->google_id == null ) {
+                $user->google_id = $google_user->getId();
+                $user->save();
+                Auth::login( $user,true );
+                return redirect()->route( 'dashboard' );
+            }
+            else {
+                Auth::login( $user,true );
+                return redirect()->route( 'dashboard' );
+            }
+        } 
     }
 }
