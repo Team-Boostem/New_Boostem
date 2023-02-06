adea798c7757cce6901acf054724a685e98dd97c
sumitchoudharySEM <sumitsem2004@gmail.com>
2023-02-06 21:26:29 +0530

    banner in the leanding page made

--- resources/views/pages/leanding.blade.php
+++ resources/views/pages/leanding.blade.php
@@ -5,6 +5,10 @@
         type="text/css" />
     <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM="
         crossorigin="anonymous"></script>
+        {{-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script> --}}
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 @endpush
 
 {{-- push styles --}}
@@ -81,6 +85,17 @@
             color: rgb(212, 212, 212);
             font-size: 0.85rem;
         }
+        .navbar{
+            padding: 0;
+        }
+        .container{
+            max-width: none;
+        }
+        .custom-banner-carousal{
+            width: 100%;
+            border-radius: 5rem;
+            box-shadow: rgba(129, 129, 129, 0.35) 0px 5px 15px;
+        }
 
         @media (max-width: 768px) {
             .subscribe-container {
@@ -147,15 +162,22 @@
             });
         });
     </script>
+    <script type="text/javascript">
+        $('.carousel').carousel({
+            interval: 3000,
+            keyboard: true,
+        })
+    </script>
 @endpush
 
 {{-- extend and yield content --}}
 @extends('layouts/community-dashboard')
 @section('content')
-    {{-- <div class="main-box">
+    <div class="main-box">
         <div id="search-status"></div>
         <div id="newsletter-status"></div>
-        <div>
+        {{-- subscribe to neawsletter........... --}}
+        {{-- <div>
             @if (subscribeNewsletterStatus())
             @else
                 <div class="subscribe-container">
@@ -175,6 +197,33 @@
                     </div>
                 </div>
             @endif
-        </div>
-    </div> --}}
+        </div> --}}
+        {{-- main heading-feature carousal --}}
+        <div id="carouselExampleIndicators" class="carousel slide custom-banner-carousal" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img class="d-block w-100" src="{{ asset('storage/user/banner/banner1.png') }}" alt="First slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="{{ asset('storage/user/banner/banner1.png') }}" alt="Second slide">
+              </div>
+              <div class="carousel-item">
+                <img class="d-block w-100" src="{{ asset('storage/user/banner/banner1.png') }}" alt="Third slide">
+              </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+    </div>
 @endsection
